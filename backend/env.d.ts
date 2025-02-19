340a9c1e72778e5ef5fe230195b3fcdcdc1b0ec7
Victor Araujo <victor_emanuel9@hotmail.com>
2025-02-19 03:37:12 -0800

    feat: update .env

--- backend/env.d.ts
+++ backend/env.d.ts
@@ -2,6 +2,8 @@ declare global {
     namespace NodeJS {
         interface ProcessEnv {
             PORT: string,
+            WEATHER_API_FORECAST: string,
+            KEY: string,
         }
     }
 }
