47e5f3e4ce197a49e414deb0db1e43160fcb4dd3
Victor Araujo <victor_emanuel9@hotmail.com>
2025-02-19 17:18:33 -0800

    feat(history): get and post

--- backend/routes/history.ts
+++ backend/routes/history.ts
@@ -1,13 +1,41 @@
 import { Router, Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
 
 const history = Router();
+const prisma = new PrismaClient();
 
+history.get("/", async (req: Request, res: Response) => {
+    try {
+        const data = await prisma.history.findMany();
+        res.status(200).json(data);
+        return;
+    } catch (error) {
+        res.status(500).send({ error: "" });
+        return;
+    }
+});
 
-history.get("", (req: Request, res: Response) => { });
+// should be /save?city=...&temp=...
+history.post("/save", async (req: Request, res: Response) => {
+    try {
+        const { city, temp } = req.query;
 
-history.post("", (req: Request, res: Response) => { });
+        await prisma.history.create({
+            data: {
+                city_name: city as string,
+                temperature: Number(temp),
+            }
+        })
 
-history.delete("", (req: Request, res: Response) => { });
+        res.status(201).json({ city, temp });
+        return;
+    } catch (error) {
+        res.status(500).send({ error: "" });
+        return;
+    }
+});
+
+history.delete("/delete/:id", (req: Request, res: Response) => { });
 
 
 
