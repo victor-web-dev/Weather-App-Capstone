90465ff7eb733657ded8ab047d82c828c64e1931
Victor Araujo <victor_emanuel9@hotmail.com>
2025-02-19 16:39:56 -0800

    change: remove console.log

--- backend/routes/forecast.ts
+++ backend/routes/forecast.ts
@@ -1,7 +1,6 @@
 import { Request, Response, Router } from "express";
 import { baseUrl } from "../http/weather-api";
 import axios, { AxiosError } from "axios";
-import "dotenv/config";
 import { IForecastMinimal } from "../types/forecast.types";
 import { weatherCleanup } from "../utils/weatherData-cleanup";
 
@@ -10,7 +9,6 @@ const forecast = Router();
 forecast.get("/:location", async (req: Request, res: Response) => {
     try {
         const { location } = req.params;
-        console.log("starting fetch");
         const { data } = await axios.get(`${baseUrl}&q=${location}&days=3`);
         const weatherMinimal: IForecastMinimal = weatherCleanup(data);
         res.status(200).json({ response: weatherMinimal });
