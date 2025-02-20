fbedfb4d5b11351290cb4dacfe0fd7b184106c59
Victor Araujo <victor_emanuel9@hotmail.com>
2025-02-19 17:28:38 -0800

    change: remove comment

--- backend/http/weather-api.ts
+++ backend/http/weather-api.ts
@@ -1,9 +1,4 @@
 import "dotenv/config";
-// import axios from "axios";
-
-// const { WEATHER_API_FORECAST, KEY } = process.env
-
-// export const weatherApi = axios.create({})
 
 const { WEATHER_API_FORECAST, KEY } = process.env;
 export const baseUrl = `${WEATHER_API_FORECAST}key=${KEY}`;
